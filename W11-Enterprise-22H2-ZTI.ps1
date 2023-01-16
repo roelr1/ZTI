d63221617bd09757a789eb17e09c5d8d7046fe03
Richard Roelofs <48410945+roelr1@users.noreply.github.com>
2023-01-16 14:42:48 +0100

    Update W11-Enterprise-22H2-ZTI.ps1

--- W11-Enterprise-22H2-ZTI.ps1
+++ W11-Enterprise-22H2-ZTI.ps1
@@ -1,5 +1,4 @@
 Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 Start-Sleep -Seconds 5
 
@@ -9,24 +8,17 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-#HyperV Tools
-Install-Module -Name PSHyperVTools -force
 
 #Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 Install-Module OSD -Force
 
-Write-Host ForegroundColor Cyan "Importing HyperV Module"
-Import-Module PSHyperVTools -Force
-
 Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 Import-Module OSD -Force
 
 #TODO: Spend the time to write a function to do this and put it here
-Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-Get-VMDvdDrive -VMName "TEST01" | Set-VMDvdDrive -Path $null
-#Dismount-DiskImage -ImagePath D:\ISO\zti\OSDCloud.iso
-Start-Sleep -Seconds 5
+#Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+#Start-Sleep -Seconds 5
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
@@ -34,10 +26,14 @@ Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage 
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-set-location -path C:\ProgramData\OSDCloud\Config\OOBEDeploy
-#Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Start-OOBEDeploy -CustomProfile https://raw.githubusercontent.com/roelr1/ZTI/main/OSDeploy.OOBEDeploy.json
-#iwr https://git.io/debloat11|iex
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$Uri = 
+irm $Uri | iex| Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+Start-OOBEDeploy
 Write-Warning "This will take a while"
 
 #Restart from WinPE
