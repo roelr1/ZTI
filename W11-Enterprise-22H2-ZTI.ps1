46ddf32687952c5a038d02388789497f7816c04f
Richard Roelofs <48410945+roelr1@users.noreply.github.com>
2023-01-16 12:18:38 +0100

    Update W11-Enterprise-22H2-ZTI.ps1

--- W11-Enterprise-22H2-ZTI.ps1
+++ W11-Enterprise-22H2-ZTI.ps1
@@ -18,7 +18,7 @@ import-Module Hyper-V.Tools -Force
 
 #TODO: Spend the time to write a function to do this and put it here
 Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-Get-VMDvdDrive -VMName "TEST01| Set-VMDvdDrive -Path $null
+Get-VMDvdDrive -VMName "TEST01" | Set-VMDvdDrive -Path $null
 #Dismount-DiskImage -ImagePath D:\ISO\zti\OSDCloud.iso
 Start-Sleep -Seconds 5
 
@@ -30,6 +30,7 @@ Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage 
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 #Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 #Start-OOBEDeploy -CustomProfile https://raw.githubusercontent.com/roelr1/ZTI/main/OSDeploy.OOBEDeploy.json
+iwr https://git.io/debloat11|iex
 Write-Warning "This will take a while"
 
 #Restart from WinPE
