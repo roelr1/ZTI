66852ac89ce63f2c3ed87f52802a95e80d7d96f9
Richard Roelofs <48410945+roelr1@users.noreply.github.com>
2023-01-16 10:23:42 +0100

    Add files via upload

---
+++ W11-Enterprise-22H2-ZTI.ps1
@@ -0,0 +1,35 @@
+Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Import-Module OSD -Force
+
+#TODO: Spend the time to write a function to do this and put it here
+Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+Get-VMDvdDrive -VMName TEST01 | Set-VMDvdDrive -Path $null
+Write-Warning "Testing with HYper V"
+#Start-Sleep -Seconds 5
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Volume -ZTI
+
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Start-OOBEDeploy
+Write-Warning "This will take a while"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
